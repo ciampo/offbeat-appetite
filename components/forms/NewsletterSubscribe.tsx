8f04009f81b73728d96928eb8bdc7db8baf6d8c1
Marco Ciampini <marcociampini@toaster.co>
2020-01-02 16:21:12 +0800

    GH-8 feat(newsletter): sample form

---
+++ components/forms/NewsletterSubscribe.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const HONEYPOT_FIELD_NAME = 'bot-bait';
+
+const NewsletterSubcribe = (): JSX.Element => {
+  return (
+    <form
+      name="newsletter"
+      method="POST"
+      data-netlify="true"
+      netlify-honeypot={HONEYPOT_FIELD_NAME}
+    >
+      <div hidden aria-hidden="true">
+        <label>
+          Don’t fill this out if you&apos;re human:
+          <input name={HONEYPOT_FIELD_NAME} />
+        </label>
+      </div>
+      <input id="name" type="text" name="name" placeholder="Name" required />
+      <input id="email" type="email" name="email" placeholder="Email Address" required />
+      <button type="submit">Subscribe</button>
+    </form>
+  );
+};
+
+export default NewsletterSubcribe;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { NextComponentType } from 'next';
 
 import DefaultPageTransitionWrapper from '../components/page-transition-wrappers/Default';
+import NewsletterSubcribe from '../components/forms/NewsletterSubscribe';
 
 const Home: NextComponentType = () => (
   <DefaultPageTransitionWrapper>
     <h1>Home</h1>
+
+    <NewsletterSubcribe />
   </DefaultPageTransitionWrapper>
 );
 
