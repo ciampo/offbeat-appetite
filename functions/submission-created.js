eaf2f2ad829f4d853254649335d00b1780185a83
Marco Ciampini <marcociampini@toaster.co>
2020-01-02 18:02:30 +0800

    GH-8 feat(form): call mailer lite APIs to add user

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -9,23 +9,25 @@ const { NEWSLETTER_API_KEY, NEWSLETTER_SUBSCRIBERS_GROUP_ID } = process.env;
 
 exports.handler = (event) => {
   const { email, name, form_name } = JSON.parse(event.body).payload;
-  console.log(`SUBMISSION: ${email} // ${name} // ${form_name}`);
+  if (form_name === 'newsletter') {
+    console.log(`NEWSLETTER SUBMISSION: [${name}] ${email}`);
 
-  return fetch(
-    `https://api.mailerlite.com/api/v2/groups/${NEWSLETTER_SUBSCRIBERS_GROUP_ID}/subscribers`,
-    {
-      method: 'GET',
-      headers: {
-        'X-MailerLite-ApiKey': NEWSLETTER_API_KEY,
-        'Content-Type': 'application/json',
-      },
-      // body: JSON.stringify({ email }),
-    }
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Response from mailer lite');
-      console.log(JSON.stringify(data, null, 2));
-    })
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+    return fetch(
+      `https://api.mailerlite.com/api/v2/groups/${NEWSLETTER_SUBSCRIBERS_GROUP_ID}/subscribers`,
+      {
+        method: 'POST',
+        headers: {
+          'X-MailerLite-ApiKey': NEWSLETTER_API_KEY,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, name }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Response from mailer lite');
+        console.log(JSON.stringify(data, null, 2));
+      })
+      .catch((error) => ({ statusCode: 422, body: String(error) }));
+  }
 };
