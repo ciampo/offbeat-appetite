c6cf80e8baa5282b7822bc44e7ba2d3be5b05298
Marco Ciampini <marcociampini@toaster.co>
2020-01-02 17:11:54 +0800

    GH-8 fix(form): add action, use "bot-field" for honeypot

--- components/forms/NewsletterSubscribe.tsx
+++ components/forms/NewsletterSubscribe.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const HONEYPOT_FIELD_NAME = 'bot-bait';
 const FORM_NAME = 'newsletter';
 
 const NewsletterSubcribe = (): JSX.Element => {
   return (
-    <form name={FORM_NAME} method="POST" data-netlify="true" netlify-honeypot={HONEYPOT_FIELD_NAME}>
+    <form
+      name={FORM_NAME}
+      method="POST"
+      action="/"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
       <input type="hidden" name="form-name" value={FORM_NAME} />
       <div hidden aria-hidden="true">
         <label>
           Don’t fill this out if you&apos;re human:
-          <input name={HONEYPOT_FIELD_NAME} />
+          <input name="bot-field" />
         </label>
       </div>
       <input id="name" type="text" name="name" placeholder="Name" required />
