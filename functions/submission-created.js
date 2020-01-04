668b75d94a606df3a18b2f2955f3d4d0cf63ce0f
Marco Ciampini <marcociampini@toaster.co>
2020-01-04 21:48:09 +0800

    GH-132 feat(function): sample slack message,  more robust error detection, return codes

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -5,32 +5,84 @@ const fetch = require('node-fetch');
 
 // Read env variables.
 require('dotenv').config();
-const { NEWSLETTER_API_KEY, NEWSLETTER_SUBSCRIBERS_GROUP_ID } = process.env;
+const { NEWSLETTER_API_KEY, NEWSLETTER_SUBSCRIBERS_GROUP_ID, SLACK_WEBHOOK_URL } = process.env;
 
-exports.handler = (event) => {
+exports.handler = async (event) => {
   const { payload } = JSON.parse(event.body);
-  console.log('FORM:', payload.form_name);
+  console.log('FORM NAME:', payload.form_name);
 
   if (payload.form_name === 'newsletter') {
-    const { email, name } = payload;
+    const email = (payload.email || '').trim();
+    const name = (payload.name || '').trim();
+
+    if (email.length === 0 || name.length === 0) {
+      const msg = 'Name and Email are mandatory fields';
+
+      console.log(`Error: ${msg}`);
+
+      return {
+        statusCode: 422,
+        body: msg,
+      };
+    }
+
     console.log(`NEWSLETTER SUBMISSION: [${name}] ${email}`);
 
-    return fetch(
-      `https://api.mailerlite.com/api/v2/groups/${NEWSLETTER_SUBSCRIBERS_GROUP_ID}/subscribers`,
-      {
-        method: 'POST',
-        headers: {
-          'X-MailerLite-ApiKey': NEWSLETTER_API_KEY,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, name }),
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Response from mailer lite');
-        console.log(JSON.stringify(data, null, 2));
-      })
-      .catch((error) => ({ statusCode: 422, body: String(error) }));
+    return (
+      // First, send user information to MailerLite
+      fetch(
+        `https://api.mailerlite.com/api/v2/groups/${NEWSLETTER_SUBSCRIBERS_GROUP_ID}/subscribers`,
+        {
+          method: 'POST',
+          headers: {
+            'X-MailerLite-ApiKey': NEWSLETTER_API_KEY,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, name }),
+        }
+      )
+        .then((response) => response.json())
+        // Catch any errors returned by MailerLite
+        .then((data) => {
+          if (!Array.isArray(data) && 'error' in data) {
+            throw new Error(`Mailerlite: ${data.error.message}`);
+          }
+          return data;
+        })
+
+        // Success: notify via slack and return HTTP 200
+        .then(() => {
+          console.log('Success!');
+
+          return fetch(SLACK_WEBHOOK_URL, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+            body: JSON.stringify({
+              text: `New submission for the newsletter form: ${name}, ${email}`,
+            }),
+          }).then(() => ({
+            statusCode: 200,
+            body: 'success',
+          }));
+        })
+
+        // Error:
+        .catch((error) => {
+          const msg = `Oops! Something went wrong:\n${error}`;
+
+          return fetch(SLACK_WEBHOOK_URL, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+            body: JSON.stringify({ text: msg }),
+          }).then(() => ({
+            statusCode: 422,
+            body: msg,
+          }));
+        })
+    );
   }
 };
