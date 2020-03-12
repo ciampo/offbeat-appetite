e765044e74346420dca0e3331b3bad31b2c0208b
Marco Ciampini <marco.ciampo@gmail.com>
2020-03-12 12:17:52 +0100

    always require external dynamicDataItems in compile-routes script

--- scripts/compile-routes.js
+++ scripts/compile-routes.js
@@ -1,10 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require('fs');
-const path = require('path');
-
-const ROOT_FOLDER = process.cwd();
-const DATA_FOLDER = path.join(ROOT_FOLDER, 'data-sanity');
-
 function compileDynamicItem({ dynamicItem, routeConfig }) {
   const { route, generateParams } = routeConfig;
 
@@ -37,18 +30,8 @@ function compileStaticItem({ routeConfig }) {
 //   content: replacedContent,
 // }
 function compileSingleRoute({ routeConfig, dynamicItemsData = null }) {
-  const { generateParams, dynamicDataType } = routeConfig;
-  if (generateParams && dynamicDataType) {
-    // If `dynamicItems` are not provided, read them from disk.
-    const dynamicItems =
-      dynamicItemsData ||
-      JSON.parse(
-        fs.readFileSync(path.join(DATA_FOLDER, `${dynamicDataType}.json`), {
-          encoding: 'utf8',
-        })
-      );
-
-    return dynamicItems.map((dynamicItem) =>
+  if (dynamicItemsData) {
+    return dynamicItemsData.map((dynamicItem) =>
       compileDynamicItem({
         routeConfig,
         dynamicItem,
