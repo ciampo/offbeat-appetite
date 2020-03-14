214269a6bfc13c8c376b0267f1ea589243c13f9e
Marco Ciampini <marco.ciampo@gmail.com>
2020-03-14 10:30:03 +0100

    Basic Home Page first version, using real data and implementing PageMeta, Accessible Image and showing category sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,75 @@
 import React from 'react';
-import { NextComponentType } from 'next';
+import { NextComponentType, GetStaticProps } from 'next';
 
+import PageMeta from '../components/PageMeta';
 import DefaultPageTransitionWrapper from '../components/page-transition-wrappers/Default';
 import NewsletterSubcribe, {
   NewsletterSubcribeVariant,
 } from '../components/forms/NewsletterSubscribe';
+import BlogPostPreview from '../components/blog-post/BlogPostPreview';
+import { AccessibleImage } from '../components/media/image';
 
-const Home: NextComponentType = () => (
-  <DefaultPageTransitionWrapper>
-    <h1>Home</h1>
+import { SanityPageHome, SanityPageHomeCategorySection } from '../typings';
 
-    <NewsletterSubcribe
-      variant={NewsletterSubcribeVariant.horizontal}
-      formInstance="subcribe-home"
-    />
-  </DefaultPageTransitionWrapper>
+// Home Category Section
+type HomeCategorySectionProps = {
+  categorySectionData: SanityPageHomeCategorySection;
+};
+const HomeCategorySection: React.FC<HomeCategorySectionProps> = ({ categorySectionData }) => (
+  <>
+    <h2>{categorySectionData.title}</h2>
+    <ul>
+      {categorySectionData.category.featuredBlogPosts.map((blogPostData) => (
+        <li key={blogPostData._id}>
+          <BlogPostPreview blogPostData={blogPostData} />
+        </li>
+      ))}
+    </ul>
+  </>
 );
 
-export default Home;
+// Home Page
+type HomeProps = {
+  homeData: SanityPageHome;
+};
+const HomePage: NextComponentType<{}, HomeProps, HomeProps> = ({ homeData }) => (
+  <>
+    <PageMeta
+      path="/"
+      title={homeData.seoTitle}
+      description={homeData.seoDescription}
+      previewImage={homeData.seoImage}
+    />
+
+    <DefaultPageTransitionWrapper>
+      <h1>{homeData.title}</h1>
+      <p>{homeData.subtitle}</p>
+
+      <AccessibleImage image={homeData.heroImage} />
+
+      <ul>
+        {homeData.categorySections.map((categorySectionData, index) => (
+          <li key={`${index}-${categorySectionData.category._id}`}>
+            <HomeCategorySection categorySectionData={categorySectionData} />
+          </li>
+        ))}
+      </ul>
+
+      <NewsletterSubcribe
+        variant={NewsletterSubcribeVariant.horizontal}
+        formInstance="subcribe-home"
+      />
+    </DefaultPageTransitionWrapper>
+  </>
+);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const homeData = await import(`../data/pageHome.json`).then((m) => m.default);
+
+  return {
+    props: {
+      homeData,
+    },
+  };
+};
+export default HomePage;
