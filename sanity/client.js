66986ebc80223105af53bb7296940b4d198af01d
Marco Ciampini <marco.ciampo@gmail.com>
2020-03-22 10:21:42 +0100

    GH-44 feat(sanity): prevent content duplication with drafts

--- sanity/client.js
+++ sanity/client.js
@@ -8,25 +8,9 @@ const DRAFT_PREFIX_LENGTH = DRAFT_PREFIX.length;
 const DRAFT_REGEX = new RegExp(`^${DRAFT_PREFIX}`);
 
 /*
-
 EXAMPLE USAGE
 const query = `*[_type == "blogPost"] {title, _id}`;
 sanityFetch(query).then((posts) => console.log(posts));
-
-
-
-NOTE FROM SANITY SUPPORT:
-
-Honestly, what you've implemented is probably the best option right now.
-Just keep in mind that if you add a limit to the query,
-you may end up not getting all the drafts within the same query.
-One option would be to do something like:
-
-{
-  "drafts": *[_type == "blogPost" && _id in path('drafts.**')],
-  "published": *[_type == "blogPost" && !(_id in path('drafts.**')] | order (_createdAt desc) | [0...100]
-}
-
 */
 
 // Setup sanity client.
@@ -38,24 +22,55 @@ const client = sanityClient({
   useCdn: false,
 });
 
+/*
+NOTE FROM SANITY SUPPORT:
+
+Honestly, what you've implemented is probably the best option right now.
+Just keep in mind that if you add a limit to the query,
+you may end up not getting all the drafts within the same query.
+One option would be to do something like:
+
+{
+  "drafts": *[_type == "blogPost" && _id in path('drafts.**')],
+  "published": *[_type == "blogPost" && !(_id in path('drafts.**')] | order (_createdAt desc) | [0...100]
+}
+*/
+
+function removeDraftDuplicates(obj, path = 'root') {
+  if (Array.isArray(obj)) {
+    // Find drafts by checking their "_id" field
+    const drafts = obj.filter((draft) => DRAFT_REGEX.test(draft._id || ''));
+
+    // Look for the non-draft version and remove it
+    drafts.forEach((draft) => {
+      const publishedDocIndex = obj.findIndex(
+        (doc) => doc._id === draft._id.substring(DRAFT_PREFIX_LENGTH)
+      );
+      if (publishedDocIndex > -1) {
+        // console.log(`Draft ${draft._id} — found duplicate in ${path}!`);
+        obj.splice(publishedDocIndex, 1);
+      }
+    });
+
+    // Recurse to one level deep
+    obj.forEach((child, i) => {
+      obj[i] = removeDraftDuplicates(child, `${path}[${i}]`);
+    });
+  } else if (typeof obj === 'object') {
+    // For objects, just keep recursing
+    for (const [key, value] of Object.entries(obj)) {
+      obj[key] = removeDraftDuplicates(value, `${path}.${key}`);
+    }
+  }
+
+  return obj;
+}
+
 async function sanityFetch(query) {
-  // Sanity fetched results can contain the draft AND published version of a document.
   const docs = await client.fetch(query);
 
-  // Drafts documents have their ID beginning with `draft.`
-  const drafts = docs.filter((draft) => DRAFT_REGEX.test(draft._id));
-
-  // For each found draft, search for the corresponding published doc, and remove it
-  drafts.forEach((draft) => {
-    const publishedDocIndex = docs.findIndex(
-      (doc) => doc._id === draft._id.substring(DRAFT_PREFIX_LENGTH)
-    );
-    if (publishedDocIndex > -1) {
-      docs.splice(publishedDocIndex, 1);
-    }
-  });
-
-  return docs;
+  // Sanity fetched results can contain the draft AND published version of a document.
+  return removeDraftDuplicates(docs);
 }
 
 module.exports = { sanityFetch };
