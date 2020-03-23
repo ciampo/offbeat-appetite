1449eebd8973699867d0a20962f155b44f476519
Marco Ciampini <marco.ciampo@gmail.com>
2020-03-23 16:08:25 +0100

    remove with-css and update postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,21 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   plugins: [
-    require('stylelint'),
-    require('postcss-easy-import'),
-    require('tailwindcss'),
-    dev
-      ? null
-      : require('@fullhuman/postcss-purgecss')({
-          content: ['./pages/**/*.{js,tsx}', './components/**/*.{js,tsx}'],
-          defaultExtractor: (content) => content.match(/[\w-:/]+(?<!:)/g) || [],
-        }),
-    require('autoprefixer'),
-    dev ? null : require('cssnano'),
-    require('postcss-reporter')({ clearReportedMessages: true }),
-  ].filter((plugin) => plugin !== null),
+    'stylelint',
+    'postcss-easy-import',
+    'tailwindcss',
+    [
+      '@fullhuman/postcss-purgecss',
+      dev
+        ? false
+        : {
+            content: ['./pages/**/*.{js,tsx}', './components/**/*.{js,tsx}'],
+            defaultExtractor: (content) => content.match(/[\w-:/]+(?<!:)/g) || [],
+          },
+    ],
+    'autoprefixer',
+    ['cssnano', dev ? false : true],
+    ['postcss-reporter', { clearReportedMessages: true }],
+  ],
 };
