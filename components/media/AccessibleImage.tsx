3c6ab46b3a38178625bc4abe5c5a92ef77858f50
Marco Ciampini <marco.ciampo@gmail.com>
2020-03-23 15:44:38 +0100

    revert import changes

--- components/media/AccessibleImage.tsx
+++ components/media/AccessibleImage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { SanityAccessibleImage } from '../../typings/media';
+import { SanityAccessibleImage } from '../../typings';
 
 type AccessibleImageProps = {
   image: SanityAccessibleImage;
