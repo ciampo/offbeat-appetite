59b86f0e86fb0f8656e7d74f5b4ac0b13005ba4f
Marco Ciampini <marco.ciampo@gmail.com>
2020-04-15 16:46:34 +0200

    feat(babel): update babel config to use the NextJS preset

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,1 @@
-// This file is needed by Jest to configure ES module imports and TypeScript
-
-const isTest = String(process.env.NODE_ENV) === 'test';
-
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-  ],
-};
+module.exports = { presets: ['next/babel'] };
