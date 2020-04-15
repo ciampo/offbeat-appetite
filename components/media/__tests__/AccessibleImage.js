f99478c12ce2dca15c94a0d44a0cb32167ee848a
Marco Ciampini <marco.ciampo@gmail.com>
2020-04-15 18:46:12 +0200

    refactor(jest): wrap RTL render in project specific render utils

--- components/media/__tests__/AccessibleImage.js
+++ components/media/__tests__/AccessibleImage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from 'offbeat-appetite-render';
+
 import AccessibleImage from '../AccessibleImage';
 import { contentFullWidthResponsiveConfig } from '../sizes-presets';
 
