796dd1e37b5b5b19f03e740e4487bf0700a9ef97
Marco Ciampini <marco.ciampo@gmail.com>
2020-04-15 16:49:40 +0200

    feat(jest): update config to follow NextJS example as much as possible

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,13 @@
 
 module.exports = {
   // moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  // collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.module\\.css$': 'identity-obj-proxy',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
   // Extend the "expect" assertions
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
