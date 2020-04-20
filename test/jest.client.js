45bf36101012faa90af004763b2c2201f6a4cec1
Marco Ciampini <marco.ciampo@gmail.com>
2020-04-20 09:50:36 +0200

    feat(test): install and configure jest-axe

--- test/jest.client.js
+++ test/jest.client.js
@@ -2,5 +2,5 @@ module.exports = {
   ...require('./jest-common.js'),
   displayName: 'client',
   testEnvironment: 'jsdom', // Extend the "expect" assertions
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', 'jest-axe/extend-expect'],
 };
