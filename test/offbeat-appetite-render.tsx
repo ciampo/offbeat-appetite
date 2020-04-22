4b49d8b90b27008b593152c22ac7cb2385f1e960
Marco Ciampini <marco.ciampo@gmail.com>
2020-04-22 15:35:34 +0200

    refactor(test): add context providers to the helper render function for tests

---
+++ test/offbeat-appetite-render.tsx
@@ -0,0 +1,52 @@
+import React, { ReactElement } from 'react';
+
+import { NextRouter } from 'next/router';
+import { RouterContext } from 'next/dist/next-server/lib/router-context';
+import { render as rtlRender, RenderOptions, RenderResult } from '@testing-library/react';
+
+import { SharingImageProvider } from '../components/meta/sharing-image-context';
+
+interface AugmentedRenderOptions extends RenderOptions {
+  router?: Partial<NextRouter>;
+}
+
+const emptyRouter: NextRouter = {
+  route: '',
+  pathname: '',
+  basePath: '',
+  query: {},
+  asPath: '',
+  push: async () => true,
+  replace: async () => true,
+  reload: () => null,
+  back: () => null,
+  prefetch: async () => undefined,
+  beforePopState: () => null,
+  isFallback: false,
+  events: {
+    on: (): null => null,
+    off: (): null => null,
+    emit: (): null => null,
+  },
+};
+
+function render(ui: ReactElement, options: AugmentedRenderOptions = {}): RenderResult {
+  const { router, ...otherOptions } = options;
+
+  const Wrapper: React.FC = ({ children }) => (
+    <RouterContext.Provider
+      value={{
+        ...emptyRouter,
+        ...router,
+      }}
+    >
+      <SharingImageProvider>{children}</SharingImageProvider>
+    </RouterContext.Provider>
+  );
+
+  return rtlRender(ui, { wrapper: Wrapper, ...otherOptions });
+}
+
+export * from '@testing-library/react';
+// override the built-in render with our own
+export { render };
