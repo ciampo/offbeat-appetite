ae69ed4049100462177ad524dd58a37592ae5e8a
Marco Ciampini <marco.ciampo@gmail.com>
2020-04-22 11:46:18 +0200

    feat(typescript): add types for navigator.share

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,12 @@
 interface Window {
   IS_GA_INIT?: boolean;
+  navigator?: { share?: (data?: ShareData) => Promise<void> };
+}
+
+type ShareOptions = { title: string; text: string; url: string };
+
+type NavigatorShare = (options: ShareOptions) => Promise<{}>;
+
+interface Navigator {
+  share?: NavigatorShare;
 }
