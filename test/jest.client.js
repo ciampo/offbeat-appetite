f435a7cd787fa5d9b4a4e636e10c212071ca468b
Marco Ciampini <marco.ciampo@gmail.com>
2020-04-22 15:34:23 +0200

    feat(jest): read .env file before running Jest tests

--- test/jest.client.js
+++ test/jest.client.js
@@ -2,5 +2,9 @@ module.exports = {
   ...require('./jest-common.js'),
   displayName: 'client',
   testEnvironment: 'jsdom', // Extend the "expect" assertions
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', 'jest-axe/extend-expect'],
+  setupFilesAfterEnv: [
+    'dotenv/config',
+    '@testing-library/jest-dom/extend-expect',
+    'jest-axe/extend-expect',
+  ],
 };
