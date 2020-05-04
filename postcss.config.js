3decd9d17f6ba52d1b63322b5f7057cc612c4185
Marco Ciampini <marco.ciampo@gmail.com>
2020-05-04 09:27:49 +0200

    refactor(postCSS): more compatible config format

--- postcss.config.js
+++ postcss.config.js
@@ -1,33 +1,30 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  plugins: [
-    'stylelint',
-    'postcss-easy-import',
-    'tailwindcss',
-    [
-      '@fullhuman/postcss-purgecss',
-      dev
-        ? false
-        : {
+  plugins: {
+    stylelint: {},
+    'postcss-easy-import': {},
+    tailwindcss: {},
+    ...(dev
+      ? {}
+      : {
+          '@fullhuman/postcss-purgecss': {
             content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
             defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
           },
-    ],
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
+        }),
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
       },
-    ],
-    ['cssnano', dev ? false : true],
-    ['postcss-reporter', { clearReportedMessages: true }],
-  ],
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+
+    ...(dev ? {} : { cssnano: {} }),
+    'postcss-reporter': { clearReportedMessages: true },
+  },
 };
