dbf65592ac98fc893508a9aea51d8449fdd8ff85
Marco Ciampini <marco.ciampo@gmail.com>
2020-05-04 10:35:30 +0200

    feat(storybook): enable typescript stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,14 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ['../stories/**/*.stories.tsx'],
+  addons: [
+    {
+      name: '@storybook/preset-typescript',
+      options: {},
+    },
+    ,
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
   webpackFinal: async (config) => {
     // do mutation to the config
 
