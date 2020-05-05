65441248bccfecb89590c1a9e65f5e5bad712d43
Marco Ciampini <marco.ciampo@gmail.com>
2020-05-05 11:29:17 +0200

    feat(storybook): icons story

---
+++ stories/3-icons.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import * as logos from '../components/icons/logos';
+import * as social from '../components/icons/social';
+
+import '../styles/index.css';
+
+export default {
+  title: 'Icons',
+};
+
+const groups = [
+  {
+    title: 'Logos',
+    icons: logos,
+  },
+  {
+    title: 'Social',
+    icons: social,
+  },
+];
+
+export const Palette: React.FC = () => (
+  <ul className="space-y-8">
+    {groups.map(({ title, icons }) => (
+      <li className="space-y-4" key={title}>
+        <h2 className="type-heading-2">{title}</h2>
+        <ul className="space-x-4">
+          {Object.values(icons).map((Icon, index) => (
+            <li key={index} className="bg-gray-lighter p-8 inline-block">
+              <Icon className="h-16 w-auto" />
+            </li>
+          ))}
+        </ul>
+      </li>
+    ))}
+  </ul>
+);
