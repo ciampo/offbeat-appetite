243afabb73087c81370a3223d6e943f06bfa1d3b
Marco Ciampini <marco.ciampo@gmail.com>
2020-05-06 08:53:49 +0200

    feat(tag): tests

--- components/tag/Tag.tsx
+++ components/tag/Tag.tsx
@@ -1,9 +1,20 @@
 import React, { memo } from 'react';
 
-type TagProps = {};
+type TagProps = {
+  className?: string;
+};
 
-const Tag: React.FC<TagProps> = memo(({ children }) => (
-  <span className="inline-flex items-center px-2 h-6 xl:px-3 xl:h-7 type-tag text-gray-dark border rounded border-gray-dark">
+const Tag: React.FC<TagProps> = memo(({ children, className }) => (
+  <span
+    className={[
+      'inline-flex items-center px-2 h-6 xl:px-3 xl:h-7',
+      'type-tag text-gray-dark border rounded border-gray-dark',
+      className,
+    ]
+      .filter(Boolean)
+      .join(' ')}
+    data-testid="tag-container"
+  >
     {children}
   </span>
 ));
---
+++ components/tag/__tests__/Tag.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { axe } from 'jest-axe';
+import { render } from 'offbeat-appetite-render';
+
+import Tag from '../Tag';
+
+const sampleContent = 'Test Tag Component';
+
+describe('Tag', () => {
+  test('renders with a valid configuration', async () => {
+    const { getByText, container } = render(<Tag>{sampleContent}</Tag>);
+
+    expect(getByText(sampleContent)).toBeInTheDocument();
+    expect(await axe(container)).toHaveNoViolations();
+  });
+
+  test('correctly adds custom classnames', async () => {
+    const testClassname = 'test-classname test-classname-two';
+    const { getByText, getByTestId, container } = render(
+      <Tag className={testClassname}>{sampleContent}</Tag>
+    );
+
+    expect(getByText(sampleContent)).toBeInTheDocument();
+    expect(getByTestId('tag-container').className).toMatch(testClassname);
+
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});
