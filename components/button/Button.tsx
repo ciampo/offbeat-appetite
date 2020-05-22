be31ebec9e4e87a94c208760ee285cb08cf1b2db
Marco Ciampini <marco.ciampo@gmail.com>
2020-05-22 17:52:56 +0200

    feat(button): custom render component, olive neutral version

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,14 +1,24 @@
 import React, { memo } from 'react';
 
-type ButtonBaseProps = {
+type DefaultButtonContainerProps = {
   disabled?: boolean;
   className?: string;
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   [key: string]: unknown;
 };
+const DefaultButtonContainer: React.FC<DefaultButtonContainerProps> = (props) => (
+  <button {...props}></button>
+);
 
-const ButtonBase: React.FC<ButtonBaseProps> = ({ className, ...props }) => (
-  <button
+type ButtonBaseProps = DefaultButtonContainerProps & {
+  component?: (props: { [key: string]: unknown }) => JSX.Element;
+};
+const ButtonBase: React.FC<ButtonBaseProps> = ({
+  className,
+  component: Component = DefaultButtonContainer,
+  ...props
+}) => (
+  <Component
     {...props}
     className={[
       'flex items-center h-10 px-4 xl:h-12 xl:px-6 rounded type-heading-4',
@@ -23,8 +33,8 @@ export const ButtonNeutral: React.FC<ButtonBaseProps> = memo(({ className, ...pr
     {...props}
     className={[
       'bg-inherit text-gray-dark border border-gray-dark',
-      'hover:shadow-md hover:bg-gray-light hover:text-gray-darker',
-      'focus:shadow-md focus:bg-gray-light focus:text-gray-darker',
+      'hover:shadow-md hover:bg-gray-dark hover:text-gray-light',
+      'focus:shadow-md focus:bg-gray-dark focus:text-gray-light',
       className,
     ]
       .filter(Boolean)
@@ -47,6 +57,21 @@ export const ButtonOlive: React.FC<ButtonBaseProps> = memo(({ className, ...prop
   />
 ));
 ButtonOlive.displayName = 'memo(ButtonOlive)';
+
+export const ButtonOliveNeutral: React.FC<ButtonBaseProps> = memo(({ className, ...props }) => (
+  <ButtonBase
+    {...props}
+    className={[
+      'bg-inherit text-olive-darker border border-olive-darker',
+      'hover:shadow-md hover:bg-olive-dark hover:text-gray-lighter',
+      'focus:shadow-md focus:bg-olive-dark focus:text-gray-lighter',
+      className,
+    ]
+      .filter(Boolean)
+      .join(' ')}
+  />
+));
+ButtonOliveNeutral.displayName = 'memo(ButtonOliveNeutral)';
 
 export const ButtonOliveInverted: React.FC<ButtonBaseProps> = memo(({ className, ...props }) => (
   <ButtonBase
