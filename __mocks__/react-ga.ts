f5e10a5fca179f1a7323b6f88018e7222d926e00
Marco Ciampini <marco.ciampo@gmail.com>
2020-05-28 13:33:36 +0200

    feat(test): add react-ga mock to allow tests to work

---
+++ __mocks__/react-ga.ts
@@ -0,0 +1,11 @@
+export default {
+  set: (): void => {
+    return undefined;
+  },
+  event: (): void => {
+    return undefined;
+  },
+  initialize: (): void => {
+    return undefined;
+  },
+};
