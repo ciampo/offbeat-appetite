913aa56a87039f8603e32758f7f6919a732b1bd0
Marco Ciampini <marco.ciampini@automattic.com>
2020-11-08 14:03:35 +0100

    Button: allow buttons to forward refs

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -12,9 +12,11 @@ type DefaultButtonContainerProps = {
   onClick?: (event: React.MouseEvent) => void;
   [key: string]: unknown;
 };
-const DefaultButtonContainer: React.FC<DefaultButtonContainerProps> = (props) => (
-  <button {...props}></button>
-);
+const DefaultButtonContainer: React.FC<DefaultButtonContainerProps> = React.forwardRef<
+  HTMLButtonElement | null,
+  DefaultButtonContainerProps
+>((props, forwardedRef) => <button {...props} ref={forwardedRef}></button>);
+DefaultButtonContainer.displayName = 'forwardRef(DefaultButtonContainer)';
 
 type ButtonBaseProps = DefaultButtonContainerProps & {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,108 +27,131 @@ type ButtonBasePropsWithBorder = ButtonBaseProps & {
   border?: boolean;
 };
 
-const ButtonBase: React.FC<ButtonBaseProps> = ({
-  className,
-  typeClassName = 'type-heading-4',
-  paddingClassName = 'px-4 xl:h-12 xl:px-6',
-  sizeClassName = 'h-10',
-  additionalHover,
-  shadow = false,
-  disableTransitions = false,
-  component: Component = DefaultButtonContainer,
-  ...props
-}) => (
-  <Component
-    {...props}
-    className={[
-      'flex items-center rounded underline-under contain-l-p',
-      'disabled:cursor-not-allowed disabled:bg-gray-medium disabled:text-gray-dark disabled:opacity-50 disabled:shadow-none focus:outline-none',
-      !disableTransitions && 'transition-bg-color-transform duration-200 ease-out',
-      additionalHover === 'underline' && 'hover:underline',
-      additionalHover === 'scaleUp' && 'hover:will-change-transform transform hover:scale-105',
-      shadow && 'hover:shadow-md focus:shadow-md',
-      paddingClassName,
-      sizeClassName,
-      typeClassName,
+const ButtonBase: React.FC<ButtonBaseProps> = React.forwardRef(
+  (
+    {
       className,
-    ].join(' ')}
-  />
+      typeClassName = 'type-heading-4',
+      paddingClassName = 'px-4 xl:h-12 xl:px-6',
+      sizeClassName = 'h-10',
+      additionalHover,
+      shadow = false,
+      disableTransitions = false,
+      component: Component = DefaultButtonContainer,
+      ...props
+    },
+    forwardedRef
+  ) => (
+    <Component
+      {...props}
+      className={[
+        'flex items-center rounded underline-under contain-l-p',
+        'disabled:cursor-not-allowed disabled:bg-gray-medium disabled:text-gray-dark disabled:opacity-50 disabled:shadow-none focus:outline-none',
+        !disableTransitions && 'transition-bg-color-transform duration-200 ease-out',
+        additionalHover === 'underline' && 'hover:underline',
+        additionalHover === 'scaleUp' && 'hover:will-change-transform transform hover:scale-105',
+        shadow && 'hover:shadow-md focus:shadow-md',
+        paddingClassName,
+        sizeClassName,
+        typeClassName,
+        className,
+      ].join(' ')}
+      ref={forwardedRef}
+    />
+  )
 );
+ButtonBase.displayName = 'forwardRef(ButtonBase)';
 
-export const ButtonOlive: React.FC<ButtonBaseProps> = ({ className, ...props }) => (
-  <ButtonBase
-    {...props}
-    className={[
-      'bg-olive-darker text-gray-white border border-olive-darker',
-      'hover:bg-olive-dark',
-      'focus:bg-olive-dark',
-      className,
-    ]
-      .filter(Boolean)
-      .join(' ')}
-  />
+export const ButtonOlive: React.FC<ButtonBaseProps> = React.forwardRef(
+  ({ className, ...props }, forwardedRef) => (
+    <ButtonBase
+      {...props}
+      className={[
+        'bg-olive-darker text-gray-white border border-olive-darker',
+        'hover:bg-olive-dark',
+        'focus:bg-olive-dark',
+        className,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+      ref={forwardedRef}
+    />
+  )
 );
+ButtonOlive.displayName = 'forwardRef(ButtonOlive)';
 
-export const ButtonTransparent: React.FC<ButtonBaseProps> = ({ className, ...props }) => (
-  <ButtonBase
-    {...props}
-    className={[
-      'bg-gray-white bg-opacity-0 text-gray-white border border-transparent',
-      'hover:bg-opacity-15',
-      'focus:bg-opacity-15',
-      className,
-    ]
-      .filter(Boolean)
-      .join(' ')}
-  />
+export const ButtonTransparent: React.FC<ButtonBaseProps> = React.forwardRef(
+  ({ className, ...props }, forwardedRef) => (
+    <ButtonBase
+      {...props}
+      className={[
+        'bg-gray-white bg-opacity-0 text-gray-white border border-transparent',
+        'hover:bg-opacity-15',
+        'focus:bg-opacity-15',
+        className,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+      ref={forwardedRef}
+    />
+  )
 );
+ButtonTransparent.displayName = 'forwardRef(ButtonTransparent)';
 
-export const ButtonOliveInverted: React.FC<ButtonBasePropsWithBorder> = ({
-  border = false,
-  className,
-  ...props
-}) => (
-  <ButtonBase
-    {...props}
-    className={[
-      // Default
-      'bg-inherit text-olive-darker',
-      border && 'border border-olive-darker',
-      // Hover
-      border ? 'hover:bg-olive-darker hover:text-gray-white' : 'hover:bg-olive-light',
-      // Focus
-      border ? 'focus:bg-olive-darker focus:text-gray-white' : 'focus:bg-olive-light',
-      className,
-    ]
-      .filter(Boolean)
-      .join(' ')}
-  />
+export const ButtonOliveInverted: React.FC<ButtonBasePropsWithBorder> = React.forwardRef(
+  ({ border = false, className, ...props }, forwardedRef) => (
+    <ButtonBase
+      {...props}
+      className={[
+        // Default
+        'bg-inherit text-olive-darker',
+        border && 'border border-olive-darker',
+        // Hover
+        border ? 'hover:bg-olive-darker hover:text-gray-white' : 'hover:bg-olive-light',
+        // Focus
+        border ? 'focus:bg-olive-darker focus:text-gray-white' : 'focus:bg-olive-light',
+        className,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+      ref={forwardedRef}
+    />
+  )
 );
+ButtonOliveInverted.displayName = 'forwardRef(ButtonOliveInverted);';
 
-export const ButtonPink: React.FC<ButtonBaseProps> = ({ className, ...props }) => (
-  <ButtonBase
-    {...props}
-    className={[
-      'bg-pink-darker text-gray-white border border-pink-darker',
-      'hover:bg-pink-dark',
-      'focus:bg-pink-dark',
-      className,
-    ]
-      .filter(Boolean)
-      .join(' ')}
-  />
+export const ButtonPink: React.FC<ButtonBaseProps> = React.forwardRef(
+  ({ className, ...props }, forwardedRef) => (
+    <ButtonBase
+      {...props}
+      className={[
+        'bg-pink-darker text-gray-white border border-pink-darker',
+        'hover:bg-pink-dark',
+        'focus:bg-pink-dark',
+        className,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+      ref={forwardedRef}
+    />
+  )
 );
+ButtonPink.displayName = 'forwardRef(ButtonPink)';
 
-export const ButtonSharing: React.FC<ButtonBaseProps> = ({ className, ...props }) => (
-  <ButtonBase
-    {...props}
-    className={[
-      'bg-transparent text-gray-dark',
-      'hover:text-gray-white',
-      'focus:text-gray-white',
-      className,
-    ]
-      .filter(Boolean)
-      .join(' ')}
-  />
+export const ButtonSharing: React.FC<ButtonBaseProps> = React.forwardRef(
+  ({ className, ...props }, forwardedRef) => (
+    <ButtonBase
+      {...props}
+      className={[
+        'bg-transparent text-gray-dark',
+        'hover:text-gray-white',
+        'focus:text-gray-white',
+        className,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+      ref={forwardedRef}
+    />
+  )
 );
+ButtonSharing.displayName = 'forwardRef(ButtonSharing)';
