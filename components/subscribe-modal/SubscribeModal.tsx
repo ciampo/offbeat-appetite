1910a9edefc65c98d6ce7a415825207a7fb04742
Marco Ciampini <marco.ciampini@automattic.com>
2020-11-08 15:39:28 +0100

    Subscribe modal close button accessibility improvements & styles

--- components/subscribe-modal/SubscribeModal.tsx
+++ components/subscribe-modal/SubscribeModal.tsx
@@ -171,10 +171,14 @@ const SubscribeModal: React.FC = () => {
           </ButtonOlive>
 
           <button
-            className="absolute top-0 right-0 p-2"
+            className={[
+              'absolute top-0 right-0 p-2',
+              'focus:bg-olive-darker focus:text-gray-white focus:outline-none',
+            ].join(' ')}
             onClick={onDismissClick}
             ref={dismissButtonRef}
             onKeyDown={onDismissKeyDown}
+            aria-label="Dismiss this modal window"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -183,8 +187,10 @@ const SubscribeModal: React.FC = () => {
               width="24"
               className="w-6 h-6 xl:w-8 xl:h-8"
             >
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+              <path
+                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+                fill="currentColor"
+              />
             </svg>
           </button>
         </div>
