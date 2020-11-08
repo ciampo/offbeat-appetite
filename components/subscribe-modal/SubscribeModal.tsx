3f3678ff0ee36ae9db2757b170d2155946d25dc4
Marco Ciampini <marco.ciampini@automattic.com>
2020-11-08 14:09:18 +0100

    SubscribeToast => SubscribeModal (with hardcoded content)

--- components/subscribe-modal/SubscribeModal.tsx
+++ components/subscribe-modal/SubscribeModal.tsx
@@ -1,55 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, memo } from 'react';
 import { useLocalStorage } from 'react-use';
 import ReactGA from 'react-ga';
 
+import { ButtonOlive } from '../button/Button';
+
 import { DISMISS_TOAST_MS_KEY } from './local-storage';
 
-const buttonCommonClassName = [
-  'type-tag bg-gray-white bg-opacity-0 text-gray-white rounded outline-none',
-  'inline-block px-4 py-2 xl:px-5 py-3',
-  'hover:bg-opacity-15',
-  'focus:bg-opacity-15',
+const SHOW_MODAL_TIMEOUT_MS = 10000;
+const ONE_DAY_MS = 1000 * 60 * 60 * 24;
+
+const ELEMENTS_TO_MAKE_ARIA_HIDDEN_WHEN_MODAL_OPENS = [
+  window?.document?.getElementById('site-header'),
+  window?.document?.getElementById('site-content'),
+  window?.document?.getElementById('site-footer'),
 ];
 
-const SHOW_TOAST_TIMEOUT_MS = 20000;
-const ONE_WEEK_MS = 1000 * 60 * 60 * 24 * 7;
+const sendModalGaEvent = (label: string): void => {
+  ReactGA.event({
+    category: 'User',
+    action: 'Interacted with Subscribe Modal',
+    label,
+  });
+};
 
-const SubscribeToast: React.FC = () => {
-  const [hideStillWaiting, setHideStillWaiting] = useState(true);
-  const [hideUserInteraction, setHideUserInteraction] = useState(false);
+const SubscribeAnchorEl: React.FC = memo((props) => <a {...props} href="#subscribe" />);
+SubscribeAnchorEl.displayName = 'memo(SubscribeAnchorEl)';
+
+const SubscribeModal: React.FC = () => {
+  const modalContainerRef = useRef<HTMLDivElement>(null);
+  const subscribeButtonRef = useRef<HTMLAnchorElement>(null);
+  const dismissButtonRef = useRef<HTMLButtonElement>(null);
+
+  const [isWaitingForInitialTimeout, setIsWaitingForInitialTimeout] = useState(true);
+  const [hasUserClikedSubscribe, sethasUserClikedSubscribe] = useState(false);
   const [dismissToastMs, setDismissToastMs] = useLocalStorage(DISMISS_TOAST_MS_KEY, 0);
 
-  const enoughTimeSinceLastDismissed = dismissToastMs + ONE_WEEK_MS < Date.now();
-  const userPrefShowToast = enoughTimeSinceLastDismissed && !hideUserInteraction;
+  const enoughTimeSinceLastDismissed = dismissToastMs + ONE_DAY_MS < Date.now();
+  const shouldShowBasedOnUserPreferences = enoughTimeSinceLastDismissed && !hasUserClikedSubscribe;
+  const shouldRender = shouldShowBasedOnUserPreferences && !isWaitingForInitialTimeout;
 
-  const onSubscribeClick = (): void => {
-    ReactGA.event({
-      category: 'User',
-      action: 'Interacted with Subscribe toast',
-      label: 'Subscribe',
-    });
-    setHideUserInteraction(true);
-  };
-  const onDismissClick = (): void => {
-    ReactGA.event({
-      category: 'User',
-      action: 'Interacted with Subscribe toast',
-      label: 'Dismiss',
-    });
-    setDismissToastMs(Date.now());
-  };
+  const hasModalBeenFocused = useRef(false);
 
+  // Initial timeout logic
   useEffect(() => {
     let timeoutId: NodeJS.Timeout | undefined;
-    if (userPrefShowToast) {
+    if (shouldShowBasedOnUserPreferences) {
       timeoutId = setTimeout(() => {
         ReactGA.event({
           category: 'Promotion',
-          action: 'Displayed Subscribe Toast',
+          action: 'Displayed Subscribe Modal',
           nonInteraction: true,
         });
-        setHideStillWaiting(false);
-      }, SHOW_TOAST_TIMEOUT_MS);
+        setIsWaitingForInitialTimeout(false);
+      }, SHOW_MODAL_TIMEOUT_MS);
     }
 
     return (): void => {
@@ -57,38 +60,132 @@ const SubscribeToast: React.FC = () => {
         clearTimeout(timeoutId);
       }
     };
-  }, [userPrefShowToast]);
+  }, [shouldShowBasedOnUserPreferences]);
 
-  return hideStillWaiting || !userPrefShowToast ? null : (
-    <div
-      className={[
-        'bg-olive-darker fixed z-30 left-0 bottom-0 w-full sm:w-auto shadow-top-right-md sm:rounded-tr',
-        'px-6 sm:px-8 xl:px-10',
-        'py-5 sm:py-6 xl:py-8',
-        'space-y-4 sm:space-y-5 xl:space-y-6',
-        'animation-fade-in-up animation-ease-out animation-.5s animation-once animation-fill-both',
-      ].join(' ')}
-    >
-      <p className="type-body text-gray-white">
-        If you enjoy this website, please support it with your subscription
-      </p>
-      <p className="space-x-6 flex items-center justify-start">
-        <a
-          className={[...buttonCommonClassName, 'border border-gray-white'].join(' ')}
-          href="#subscribe"
-          onClick={onSubscribeClick}
-        >
-          Subscribe
-        </a>
-        <button
-          className={[...buttonCommonClassName, 'font-medium border-0'].join(' ')}
-          onClick={onDismissClick}
-        >
-          Dismiss
-        </button>
-      </p>
-    </div>
-  );
+  // Click events
+  const onSubscribeClick = (): void => {
+    sendModalGaEvent('Subscribe button');
+    sethasUserClikedSubscribe(true);
+  };
+  const onDismissClick = (): void => {
+    sendModalGaEvent('Dismiss button');
+    setDismissToastMs(Date.now());
+  };
+  const onScrimClick = (): void => {
+    sendModalGaEvent('Scrim interaction');
+    setDismissToastMs(Date.now());
+  };
+
+  // Keyboard events: trap focus between subscribe and dismiss buttons
+  const onSubscribeKeyDown = (e: React.KeyboardEvent<HTMLAnchorElement>): void => {
+    if (e.key === 'Tab' && e.shiftKey) {
+      dismissButtonRef.current?.focus();
+      e.preventDefault();
+    }
+  };
+  const onDismissKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>): void => {
+    if (e.key === 'Tab' && !e.shiftKey) {
+      subscribeButtonRef.current?.focus();
+      e.preventDefault();
+    }
+  };
+
+  // Keyboard events: press ESC to dismiss modal
+  const onModalKeyDown = (e: React.KeyboardEvent<HTMLDivElement>): void => {
+    if (e.key === 'Esc') {
+      sendModalGaEvent('ESC key');
+      setDismissToastMs(Date.now());
+      e.preventDefault();
+    }
+  };
+
+  // Prevent body from scrolling when modal is visible
+  useEffect(() => {
+    document.body.style.overflow = shouldRender ? 'hidden' : '';
+  }, [shouldRender]);
+
+  // Set aria-hidden on other parts of the page to exclude them from the
+  // screen reader while the modal is open
+  useEffect(() => {
+    ELEMENTS_TO_MAKE_ARIA_HIDDEN_WHEN_MODAL_OPENS.map((el) => {
+      if (el) {
+        el.setAttribute('aria-hidden', shouldRender ? 'true' : 'false');
+      }
+    });
+  }, [shouldRender]);
+
+  // Move focus on the modal when it's shown
+  useEffect(() => {
+    if (shouldRender && modalContainerRef.current && !hasModalBeenFocused.current) {
+      modalContainerRef.current.focus();
+      hasModalBeenFocused.current = true;
+    }
+  }, [shouldRender]);
+
+  return shouldRender ? (
+    <>
+      <div
+        aria-hidden="true"
+        className="fixed z-50 left-0 top-0 h-full w-full bg-black opacity-50"
+        onClick={onScrimClick}
+      />
+      <div
+        className={[
+          'fixed z-50 transform-translate-center max-w-lg xl:max-w-xl',
+          'flex flex-col-reverse xsm:flex-row-reverse bg-white focus:outline-none',
+        ].join(' ')}
+        style={{
+          width: 'calc(100% - 3rem)',
+        }}
+        role="dialog"
+        aria-labelledby="modal-title"
+        aria-describedby="modal-descr"
+        tabIndex={-1}
+        ref={modalContainerRef}
+        onKeyDown={onModalKeyDown}
+      >
+        <div className="p-4 xsm:pt-8 xl:py-12 xl:px-6 xl:pb-6">
+          <h2 id="modal-title" className="type-display-3 mb-2 xsm:mb-3 xl:mb-4">
+            The 2020 Sustainable Holiday Gift Guide is out!
+          </h2>
+          <p id="modal-descr" className="type-body mb-4 xsm:mb-6 xl:mb-8">
+            Subscribe now to get a copy in your inbox!
+          </p>
+
+          <ButtonOlive
+            href="#subscribe"
+            onClick={onSubscribeClick}
+            ref={subscribeButtonRef}
+            onKeyDown={onSubscribeKeyDown}
+            className="mx-auto w-full justify-center"
+          >
+            Subscribe
+          </ButtonOlive>
+
+          <button
+            className="absolute top-0 right-0 p-2"
+            onClick={onDismissClick}
+            ref={dismissButtonRef}
+            onKeyDown={onDismissKeyDown}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 0 24 24"
+              width="24"
+              className="w-6 h-6 xl:w-8 xl:h-8"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+            </svg>
+          </button>
+        </div>
+        <div className="flex-shrink-0 flex-grow-0 w-full h-20 xsm:w-48 xsm:h-auto xsm:self-stretch xl:w-56 bg-gray-400">
+          img
+        </div>
+      </div>
+    </>
+  ) : null;
 };
 
-export default SubscribeToast;
+export default SubscribeModal;
