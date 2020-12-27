b919a284773d0adf104f3d029e47bf212e75ac6b
Marco Ciampini <marco.ciampo@gmail.com>
2020-12-27 09:37:47 +0100

    Do not render Subscribe Modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,9 +15,6 @@ import { SharingImageProvider } from '../components/meta/sharing-image-context';
 import MainLayout from '../components/layouts/Main';
 
 const Analytics = dynamic(() => import('../components/Analytics'), { ssr: false });
-const SubscribeModal = dynamic(() => import('../components/subscribe-modal/SubscribeModal'), {
-  ssr: false,
-});
 
 import { NextComponentTypeWithLayout } from '../typings';
 
@@ -34,8 +31,6 @@ export default class MyApp extends App {
         <Layout>
           <Component {...pageProps} key={router.asPath} />
         </Layout>
-
-        <SubscribeModal />
       </SharingImageProvider>
     );
   }
