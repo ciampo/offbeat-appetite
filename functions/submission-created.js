76a4a12b1c4ba60405d24986fda6aefe0c317f0a
Marco Ciampini <marco.ciampo@gmail.com>
2023-04-24 23:36:33 +0200

    try some MAGIC

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const sanityClient = require('@sanity/client');
 
 // Read env variables.
